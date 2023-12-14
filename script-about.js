e1c1e1b3ce12c96c860b7d7f092220978f037861
kamarini <>
2023-12-14 23:42:11 +0100

    end for today

--- script-about.js
+++ script-about.js
@@ -1,48 +1,48 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const images = document.querySelectorAll(".image-container img");
-    const popup = document.querySelector(".popup");
-    const popupTitle = document.querySelector(".popup h3");
-    const popupContent = document.querySelector(".popup p");
-    const closeBtn = document.querySelector(".popup-close-btn");
-    const overlay = document.querySelector(".overlay");
-    const mainContent = document.querySelector("main"); /* Update with your main content selector */
-  
-    images.forEach((image) => {
-      image.addEventListener("click", () => {
-        if (image.classList.contains("nso")) {
-          popupTitle.textContent = "NSO";
-          popupContent.textContent = "Behind the Scenes: NSOs play a crucial role in tracking scores, penalties, and timing during matches. They're volunteers and a vital part of the game day experience.";
-        } else if (image.classList.contains("skaters")) {
-          popupTitle.textContent = "SKATERS";
-          popupContent.textContent = "Did you know? Our skaters train relentlessly to improve their skills. We have practice sessions three times a week to perfect our strategy and teamwork.";
-        } else if (image.classList.contains("ref")) {
-          popupTitle.textContent = "REFS";
-          popupContent.textContent = "Fun Fact: A game requires a minimum of 10 referees to ensure fair play. Our referees come from various clubs, bringing diverse expertise to the game.";
-        }
-        else if (image.classList.contains("commentators-2")) {
-          popupTitle.textContent = "COMMENTATORS";
-          popupContent.textContent = "Meet Our Voices: Commentators add excitement and context to the game, explaining the action to the audience. They're passionate volunteers who amplify the energy of the game.";
-        } else if (image.classList.contains("merch-bw")) {
-          popupTitle.textContent = "MERCH STAND";
-          popupContent.textContent = "Support Our Club: Run by dedicated volunteers, our merch stand offers a variety of items. Purchasing merch supports our club and helps fundraise for our activities.";
-        }
-        else if (image.classList.contains("penalty")) {
-          popupTitle.textContent = "PENALTY BOX";
-          popupContent.textContent = "Understanding Penalties: This is where skaters serve time for rule infractions. Penalties vary in severity and impact gameplay strategy.";
-        } 
-  
-        // Show the popup and overlay
-        popup.classList.add("active");
-        overlay.classList.add("active");
-      });
-    });
-  
-    closeBtn.addEventListener("click", () => {
-      // Close the popup and overlay
-      popup.classList.remove("active");
-      overlay.classList.remove("active");
+  const images = document.querySelectorAll(".image-container img");
+  const popup = document.querySelector(".popup");
+  const popupTitle = document.querySelector(".popup h3");
+  const popupContent = document.querySelector(".popup p");
+  const closeBtn = document.querySelector(".popup-close-btn");
+  const overlay = document.querySelector(".overlay");
+  const mainContent = document.querySelector("main"); /* Update with your main content selector */
+
+  images.forEach((image) => {
+    image.addEventListener("click", () => {
+      if (image.classList.contains("nso")) {
+        popupTitle.textContent = "NSO";
+        popupContent.textContent = "Behind the Scenes: NSOs play a crucial role in tracking scores, penalties, and timing during matches. They're volunteers and a vital part of the game day experience.";
+      } else if (image.classList.contains("skaters")) {
+        popupTitle.textContent = "SKATERS";
+        popupContent.textContent = "Did you know? Our skaters train relentlessly to improve their skills. We have practice sessions three times a week to perfect our strategy and teamwork.";
+      } else if (image.classList.contains("ref")) {
+        popupTitle.textContent = "REFS";
+        popupContent.textContent = "Fun Fact: A game requires a minimum of 10 referees to ensure fair play. Our referees come from various clubs, bringing diverse expertise to the game.";
+      }
+      else if (image.classList.contains("commentators-2")) {
+        popupTitle.textContent = "COMMENTATORS";
+        popupContent.textContent = "Meet Our Voices: Commentators add excitement and context to the game, explaining the action to the audience. They're passionate volunteers who amplify the energy of the game.";
+      } else if (image.classList.contains("merch-bw")) {
+        popupTitle.textContent = "MERCH STAND";
+        popupContent.textContent = "Support Our Club: Run by dedicated volunteers, our merch stand offers a variety of items. Purchasing merch supports our club and helps fundraise for our activities.";
+      }
+      else if (image.classList.contains("penalty")) {
+        popupTitle.textContent = "PENALTY BOX";
+        popupContent.textContent = "Understanding Penalties: This is where skaters serve time for rule infractions. Penalties vary in severity and impact gameplay strategy.";
+      } 
+
+      // Show the popup and overlay
+      popup.classList.add("active");
+      overlay.classList.add("active");
     });
   });
+
+  closeBtn.addEventListener("click", () => {
+    // Close the popup and overlay
+    popup.classList.remove("active");
+    overlay.classList.remove("active");
+  });
+});
   
 
 
